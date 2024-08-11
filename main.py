1821a192f3460eb77768b62bfb1d8cf709050a7d
Itanaã Machado de Oliveira <35381794-itanaaoliveira@users.noreply.replit.com>
2024-08-11 22:29:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+print("Hello World!")
+print('Meu nome é Itanaã')
+
+#isso aqui eh um comentario
+print("Hello World \n \tMeu nome eh Itanaa")
+
+"""
+Isso eh um comentario de mais de uma linha
+"""
+
+#exemplo de quebra de linha
+print("""Hello World!
+Meu nome eh Itanaa""")
+
+#exemplo de variavel
+
+texto_para_imprimir = """Hello World!
+Meu nome Itanaa"""
+
+print(texto_para_imprimir, "Machado de Oliveira")
+
+#calculo da area do circulo
+pi = 3.1415
+raio = 2.2
+area_do_circulo = pi * raio * raio
+
+print("A área do circulo é =", area_do_circulo, "cm2")
